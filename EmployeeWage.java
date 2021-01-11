eb968bc913f18cf28d9cc18f767742606f08bb49
Aanchalt14 <aanchalt71@gmail.com>
2021-01-12 00:24:18 +0530

    Add : Refactor the code using class

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -116,6 +116,32 @@ public class EmployeeWage {
    totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
    System.out.println("Total emp wage : " +totalEmpWage);
  }
+   //UC-7 Refactor using class
+   public static int computeEmpWage() {
+	  //Variables
+	   int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+	   //Computation
+	   while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+		  totalWorkingDays++;
+	   int empCheck =(int)Math.floor(Math.random()*10)%3;
+	   switch (empCheck) {
+	     case IS_PART_TIME :
+	    	  empHrs=4;
+	    	  break;
+	     case IS_FULL_TIME :
+	    	  empHrs=8;
+	     default:
+	    	  empHrs=0;
+	   }
+	   totalEmpHrs += empHrs;
+	   System.out.println("Day#: " +totalWorkingDays + "Emp Hr : " +empHrs);
+	  }
+	  int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
+	  System.out.println("Total Emp Wage :" +totalEmpWage);
+	  return totalEmpWage;
+	 }
 }
+   
+
 
 
