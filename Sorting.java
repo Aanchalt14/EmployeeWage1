c74d06f3f74a7ecec26e8d9a7caa13a8699b9927
Aanchalt14 <66659745+Aanchalt14@users.noreply.github.com>
2021-01-19 12:52:06 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,48 @@
+package com.day0.addressbook;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+
+public abstract class Sorting implements IAddressBookService {
+	
+	public void sortbypersonName() {
+		  Comparator<Contacts> cm2=Comparator.comparing(Contacts::getFirstName);  
+		   Collections.sort(arraylist,cm2);  
+		      System.out.println("Sorting by personName>>>>>");  
+		      for(Contacts st: arraylist){  
+		        System.out.println(st);  
+		        System.out.println("*************************");
+		        }    
+		      
+	}
+
+	public void sortbyzip() {
+		 Comparator<Contacts> cm2=Comparator.comparing(Contacts::getZip);  
+		   Collections.sort(arraylist,cm2);  
+		      System.out.println("Sorting by zip>>>>>>>>>>");  
+		      for(Contacts st: arraylist){  
+		        System.out.println(st);  
+		        System.out.println("********************");
+	}
+}
+	public void sortbycity() {
+		 Comparator<Contacts> cm2=Comparator.comparing(Contacts::getCity);  
+		   Collections.sort(arraylist,cm2);  
+		      System.out.println("Sorting by city>>>>>>>>>");  
+		      for(Contacts st: arraylist){  
+		        System.out.println(st); 
+		        System.out.println("**********************");
+	}
+}
+	public void sortbystate() {
+		 Comparator<Contacts> cm2=Comparator.comparing(Contacts::getState);  
+		   Collections.sort(arraylist,cm2);  
+		      System.out.println("Sorting by state>>>>>>>>>>");  
+		      for(Contacts st: arraylist){  
+		        System.out.println(st);  
+		        System.out.println("********************");
+	}
+}
+
+}
